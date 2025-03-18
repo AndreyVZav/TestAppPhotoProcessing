a27db3324b0c43533997e1df61ffc19bd2e8dc96
AndreyVZav <zav.av@bk.ru>
2025-03-18 17:31:40 +0300

    start

---
+++ TestAppPhotoProcessing/TestAppPhotoProcessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppPhotoProcessingApp.swift
+//  TestAppPhotoProcessing
+//
+//  Created by Андрей Завадский on 18.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAppPhotoProcessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
