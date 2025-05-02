a06b351b27367d6685c27f4af4dba372b9eb908a
AndreyVZav <zav.av@bk.ru>
2025-05-02 17:37:26 +0300

    add loading

--- TestAppPhotoProcessing/AppRoot/AppRootViewModel.swift
+++ TestAppPhotoProcessing/AppRoot/AppRootViewModel.swift
@@ -9,6 +9,8 @@ import Combine
 
 class AppRootViewModel: ObservableObject {
     @Published var isAuthenticated = false
+    @Published var isLoading = true
+    
     let authContainerViewModel: AuthContainerViewModel
     let imageEditorViewModel = ImageEditorViewModel()
     
@@ -25,6 +27,13 @@ class AppRootViewModel: ObservableObject {
         self.authContainerViewModel.onAuthSuccess = { [weak self] in
             self?.setAuthenticated(true)
         }
+        
+        // Симуляция проверки авторизации (например, из Keychain)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            // здесь могла бы быть реальная проверка
+            self.isLoading = false
+        }
+        
     }
     
     func setAuthenticated(_ isAuthenticated: Bool) {
